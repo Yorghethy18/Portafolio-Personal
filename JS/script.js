3f32a2328e3833cd488e9ca6b935449488549525
Yorghet Hernandez <1392696@senati.pe>
2023-03-20 17:19:46 -0500

    JS ACTUALIZADO

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+const miImagen = document.getElementById('mi-imagen');
+
+miImagen.addEventListener('click', function() {
+  // Redireccionar a otra página
+  alert("Bienvenido");
+  window.location.href = 'https://www.senati.edu.pe/?nocache=1';
+});
+
+
